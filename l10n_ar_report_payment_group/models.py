036f2656f8a1f490d888a2ca2006fccf84de84ab
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2019-11-08 10:31:06 -0300

    Fix account payment group print button 3

--- l10n_ar_report_payment_group/models.py
+++ l10n_ar_report_payment_group/models.py
@@ -7,5 +7,4 @@ class AccountPaymentGroup(models.Model):
     @api.multi
     def payment_print(self):
         report = self.env['ir.actions.report']._get_report_from_name('l10n_ar_report_payment_group.report_payment_group')
-        for apg in self:
-            return report.report_action(docids=[apg.id])
+        return report.report_action(self)
