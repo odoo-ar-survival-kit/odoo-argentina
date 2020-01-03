7559c18b3e5396aa9320eb98fae7f9d947553ff4
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2020-01-02 21:15:21 -0300

    Fixing l10n_ar_afipws_fe ncm

--- l10n_ar_afipws_fe/models/product.py
+++ l10n_ar_afipws_fe/models/product.py
@@ -2,15 +2,10 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from .pyi25 import PyI25
 from odoo import fields, models, api, _
 from odoo.exceptions import UserError
-import base64
-from io import BytesIO
 import logging
-import sys
-import traceback
-from datetime import datetime
+
 _logger = logging.getLogger(__name__)
 
 
